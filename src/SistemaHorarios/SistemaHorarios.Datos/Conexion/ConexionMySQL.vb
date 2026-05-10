dbc3a2938f443f281f2b2e0e7e48b042efa3afcb
NataliaVelez0903 <natalia.velez.orjuela@gmail.com>
2026-05-09 21:26:32 -0500

    feat: configurar mysql docker y conexion inicial de datos

---
+++ src/SistemaHorarios/SistemaHorarios.Datos/Conexion/ConexionMySQL.vb
@@ -0,0 +1,20 @@
+﻿Imports MySql.Data.MySqlClient
+
+Namespace Conexion
+
+    Public Class ConexionMySQL
+
+        Private cadenaConexion As String =
+            "server=localhost;" &
+            "port=3306;" &
+            "database=sistema_horarios;" &
+            "uid=root;" &
+            "pwd=1234;"
+
+        Public Function ObtenerConexion() As MySqlConnection
+            Return New MySqlConnection(cadenaConexion)
+        End Function
+
+    End Class
+
+End Namespace
